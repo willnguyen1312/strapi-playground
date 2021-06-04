421d0e23f50ab7fcd00f16e12a27ec595eab3f18
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-04 17:59:11 +0700

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1e59d84cc0523784df7e6a0854fd3636'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e3bce3d3-b50f-42f4-8a42-baeb40c191f1'
+};
